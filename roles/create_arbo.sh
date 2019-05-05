625277060f8905f2b3ba7c444e08ef35f365e2c7
GregLebreton <root@localhost.localdomain>
2019-05-05 14:24:53 +0200

    ajout roles appserver1 et webserver1

---
+++ roles/create_arbo.sh
@@ -0,0 +1,2 @@
+# mkdir -p inventories inventories/PROD inventories/PROD/host_vars inventories/PROD/group_vars inventories/DEV/ inventories/DEV/group_vars inventories/DEV/host_vars library module_utils filter_plugins roles roles/common roles/webtier roles/monitoring roles/
+mkdir -p defaults handlers meta tasks tests vars 
